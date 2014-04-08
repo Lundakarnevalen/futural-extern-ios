ad93f867570c866c99974d2c2292867d0de1e188
Richard Luong <richardluong92@gmail.com>
2014-04-08 15:24:11 +0200

    init

---
+++ futural-extern-ios/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  futural-extern-ios
+//
+//  Created by Richard Luong on 2014-04-08.
+//  Copyright (c) 2014 Richard Luong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
